3d3dc6e715fd0358c0cb45b06c38d7f7145dfda7
cuong.ntt <thecuongthehieu@gmail.com>
2025-07-03 11:06:20 +0200

    Add more tests

--- client/python-client/src/main.py
+++ client/python-client/src/main.py
@@ -2,8 +2,11 @@ from query_client import QueryDecorator, QueryClient
 import timeit
 import math
 
+SERVER = "http://localhost:5001"
+
+
 @QueryDecorator
-def maxCurrent(shot: int) -> dict:
+def computeMaxCurrent(shot: int) -> dict:
     import MDSplus as mds
     import numpy as np
 
@@ -19,32 +22,46 @@ def maxCurrent(shot: int) -> dict:
     return result
 
 if __name__ == "__main__":
-    SERVER = "http://localhost:5001"
-
     # Query Creation
     response = QueryClient.createQuery(server=SERVER, queryName="computeMaxCurrent", 
                                        dependencies=[], queryDescription="", 
-                                       executionUnitFunction=maxCurrent)
+                                       executionUnitFunction=computeMaxCurrent)
 
     # Query Execution
-    start_ts = timeit.default_timer()
-    result = QueryClient.execute(SERVER, "computeMaxCurrent", [39384, 39385, 39386, 39387, 39388, 39389, 39390, 39391])
-    elapsed_time = timeit.default_timer() - start_ts
-    print("Running time = {} ms".format(math.ceil(elapsed_time * 1000)))
+    result = QueryClient.execute(SERVER, 
+                                queryName="computeMaxCurrent", 
+                                shotList=[39384, 39385, 39386, 39387, 39388, 39389, 39390, 39391])
 
-    # Query Dependency
-    start_ts = timeit.default_timer()
-    result = QueryClient.execute(SERVER, "computeMaxCurrent", [39384, 39385, 39386, 39387, 39388, 39389, 39390, 39391])
-    elapsed_time = timeit.default_timer() - start_ts
-    print("Running time of computeMaxCurrent  = {} ms".format(math.ceil(elapsed_time * 1000)))
+
+    # # Query Dependency
+    # start_ts = timeit.default_timer()
+    # result = QueryClient.execute(SERVER, "computeMaxCurrent", [39384, 39385, 39386, 39387, 39388, 39389, 39390, 39391])
+    # elapsed_time = timeit.default_timer() - start_ts
+    # print("Running time of computeMaxCurrent  = {} ms".format(math.ceil(elapsed_time * 1000)))
+
+    # start_ts = timeit.default_timer()
+    # result = QueryClient.execute(SERVER, "computeMinCurrent", [39384, 39385, 39386, 39387, 39388, 39389, 39390, 39391])
+    # elapsed_time = timeit.default_timer() - start_ts
+    # print("Running time of computeMinCurrent = {} ms".format(math.ceil(elapsed_time * 1000)))
+
+    # start_ts = timeit.default_timer()
+    # result = QueryClient.execute(SERVER, "computeMaxAndMinCurrent", [39384, 39385, 39386, 39387, 39388, 39389, 39390, 39391])
+    # elapsed_time = timeit.default_timer() - start_ts
+    # print("Running time of computeMaxAndMinCurrent = {} ms".format(math.ceil(elapsed_time * 1000)))
+
 
     start_ts = timeit.default_timer()
-    result = QueryClient.execute(SERVER, "computeMinCurrent", [39384, 39385, 39386, 39387, 39388, 39389, 39390, 39391])
+    result = QueryClient.execute(SERVER, "getSpectrum", [30873, 30874, 30876, 30878])
     elapsed_time = timeit.default_timer() - start_ts
-    print("Running time of computeMinCurrent = {} ms".format(math.ceil(elapsed_time * 1000)))
+    print("Running time of getSpectrum = {} ms".format(math.ceil(elapsed_time * 1000)))
 
     start_ts = timeit.default_timer()
-    result = QueryClient.execute(SERVER, "computeMaxAndMinCurrent", [39384, 39385, 39386, 39387, 39388, 39389, 39390, 39391])
+    result = QueryClient.execute(SERVER, "getMode7Ratio", [30873, 30874, 30876, 30878])
     elapsed_time = timeit.default_timer() - start_ts
-    print("Running time of computeMaxAndMinCurrent = {} ms".format(math.ceil(elapsed_time * 1000)))
+    print("Running time of getMode7Ratio = {} ms".format(math.ceil(elapsed_time * 1000)))
+
+    start_ts = timeit.default_timer()
+    result = QueryClient.execute(SERVER, "getMode7DominationRatio", [30873, 30874, 30876, 30878])
+    elapsed_time = timeit.default_timer() - start_ts
+    print("Running time of getMode7DominationRatio = {} ms".format(math.ceil(elapsed_time * 1000)))
 
